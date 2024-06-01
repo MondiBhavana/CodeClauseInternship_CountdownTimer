6e1d7ee6b8087834ef738f384b2257c01fa48e10
MondiBhavana <bhavanamondi23@gmail.com>
2024-06-01 14:47:26 +0530

    React

---
+++ src/CountdownTimer.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./CountdownTimer.css";
+
+const CountdownTimer = ({ targetDate, eventName }) => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span key={interval}>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div className="countdown-timer">
+      <h1>{eventName}</h1>
+      <div className="timer">
+        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
